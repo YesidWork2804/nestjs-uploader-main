3aa7e900230bce9b8afeddf60bb77c4dc1629e1d
YesidWork2804 <yesidwork2804@gmail.com>
2023-10-20 12:43:54 -0500

    cambio balanceador

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Cambia esto según tu origen permitido
+    origin: 'http://loadbalancer-1025959931.us-east-2.elb.amazonaws.com:3000', // Cambia esto según tu origen permitido
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
